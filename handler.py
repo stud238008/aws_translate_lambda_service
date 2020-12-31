0a4e9e880f9357464d091a7a11400b27b8bc7d9f
eipc16 <pprzemek.312@gmail.com>
2020-12-31 01:15:04 +0100

    removed unnecessary body

--- handler.py
+++ handler.py
@@ -1,25 +1,14 @@
-import json
 import boto3
 
 translate_api = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
 
 def translate(event, context):
-    request_body = json.loads(event['body'])
-    
-    text = request_body['text']
-    source_language = request_body['source_language']
-    target_language = request_body['target_language']
+    text = event['text']
+    source_language, target_language = event['sourceLang'], event['targetLang']
 
     result = translate_api.translate_text(Text=text, 
             SourceLanguageCode=source_language, TargetLanguageCode=target_language)
     
-    body = {
+    return {
         "text": result.get('TranslatedText')
     }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    
-    return response
