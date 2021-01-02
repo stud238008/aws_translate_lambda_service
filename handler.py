001ed00e325ae6b7ccc0753b8c8847e9c7bc9195
eipc16 <pprzemek.312@gmail.com>
2021-01-02 16:41:25 +0100

    add confidence placeholder

--- handler.py
+++ handler.py
@@ -1,4 +1,5 @@
 import boto3
+import random
 
 translate_api = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
 
@@ -9,6 +10,12 @@ def translate(event, context):
     result = translate_api.translate_text(Text=text, 
             SourceLanguageCode=source_language, TargetLanguageCode=target_language)
     
+    # Set source text as seed, so confidence is always the same for given text
+    random.seed(text)
+
     return {
-        "text": result.get('TranslatedText')
+        "result": result.get('TranslatedText'),
+        "confidence": random.uniform(70, 100),
+        "sourceLang": source_language,
+        "targetLang": target_language
     }
